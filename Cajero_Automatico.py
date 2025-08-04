fc977347bb7266a76a558bcc672fe70c0b802260
Eidder Andres Ardila Pita <eidaardila@soy.sena.edu.co>
2025-08-04 16:38:40 -0500

    Se agrego el ejercicio 14=Cajero automatico

---
+++ Cajero_Automatico.py
@@ -0,0 +1,88 @@
+"""
+Crea un programa que simule las operaciones de un cajero automático. El
+programa debe gestionar un saldo inicial.
+
+• Debe encapsular la lógica en funciones como consultar_saldo(),
+depositar(), retirar().
+• Utiliza un bucle while para mantener el programa en ejecución hasta que el
+usuario decida salir.
+• Valida las operaciones (ej. no se puede retirar más dinero del que hay en el
+saldo).
+• El código debe ser claro, legible y seguir las recomendaciones de PEP 8.
+• Conceptos integrados: Funciones, bucle while, condicionales, variables,
+operadores, I/O, PEP 8.
+"""
+opcion = 0
+saldo = 0.0
+
+
+def depositar():
+    global saldo  # Declaramos que usaremos la variable global 'saldo'
+    print("---------------------")
+    monto = float(input("Ingrese el monto a depositar: $ "))
+
+    if monto > 0:
+        saldo += monto
+        print(f"Depósito exitoso. Nuevo saldo: ${saldo:.2f}")
+    else:
+        print("El monto debe ser mayor que cero.")
+
+
+def consultar_saldo():
+    global saldo
+    print("-----------------------------------------")
+    print("|\t\t\t\t\t\t\t\t\t\t|")
+    print("|\t\t\t\t\t\t\t\t\t\t|")
+    print(f"|\tSu saldo actual es $ {saldo:.2f}\t\t|")
+    print("|\t\t\t\t\t\t\t\t\t\t|")
+    print("|\t\t\t\t\t\t\t\t\t\t|")
+    print("-----------------------------------------")
+
+
+def retirar():
+    global saldo
+    print("---------------------")
+    saldo_retiro = float(input("Ingrese el monto a retirar: $ "))
+
+    if saldo_retiro > 0:
+        if saldo_retiro <= saldo:
+            saldo -= saldo_retiro
+            print(f"Retiro exitoso. Nuevo saldo: ${saldo:.2f}")
+        else:
+            print("Fondos insuficientes.")
+    else:
+        print("El monto debe ser mayor que cero.")
+
+
+print("-------------------------------")
+print("Bienvenido al cajero :)")
+print("-------------------------------")
+while True:
+
+    print("1. Depositar\n"
+          "2. Consultar Saldo\n"
+          "3. Retirar\n"
+          "4. Salir")
+
+    opcion = int(input("Ingrese la opcion: "))
+
+    match opcion:
+        case 1:
+            depositar()
+        case 2:
+            consultar_saldo()
+        case 3:
+            retirar()
+        case 4:
+            respuesta = input("Esta seguro que desea salir (si o no): ").lower()
+            if respuesta == "si":
+                print("Saliendo...")
+                break
+            else:
+                print("Volviendo al menu Principal")
+        case _:
+            print("Opción no válida. Por favor, ingrese una opción del 1 al 4.")
+print("===================================================================")
+print("Gracias por utilizar el cajero. ¡Hasta luego! :)")
+print("===================================================================")
+
